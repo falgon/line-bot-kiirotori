e8421e900bf91c4a88d57f47795cde710c079b0c
roki <falgon53@yahoo.co.jp>
2023-11-08 07:25:51 +0900

    refactoring #2

--- src/LBKiirotori/Webhook/EventHandlers/Ext/SendPlain.hs
+++ src/LBKiirotori/Webhook/EventHandlers/Ext/SendPlain.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, TemplateHaskell #-}
 module LBKiirotori.Webhook.EventHandlers.Ext.SendPlain (
     sendPlain
 ) where
@@ -7,6 +6,7 @@ module LBKiirotori.Webhook.EventHandlers.Ext.SendPlain (
 import           Control.Monad.Logger                           (logError,
                                                                  logInfo)
 import           Control.Monad.Parallel                         (bindM2)
+import           Control.Monad.Trans.Maybe                      (MaybeT (..))
 
 import           LBKiirotori.AccessToken                        (getAccessToken)
 import           LBKiirotori.API.PushMessage
@@ -14,27 +14,26 @@ import           LBKiirotori.Config                             (LBKiirotoriAppC
                                                                  LBKiirotoriConfig (..))
 import           LBKiirotori.Data.MessageObject                 (MessageBody (..),
                                                                  textMessage)
-import           LBKiirotori.Internal.Utils                     (tshow)
+import           LBKiirotori.Internal.Utils                     (hoistMaybe,
+                                                                 tshow)
 import           LBKiirotori.Webhook.EventObject.Core           (ExtEventObject (..),
                                                                  ExtEventType (..),
                                                                  LineEventObject)
 import           LBKiirotori.Webhook.EventObject.LineBotHandler
 
+
 sendPlain :: ExtEventObject
     -> LineBotHandler ()
 sendPlain x
-    | extEventType x == ExtEventTypeSendPlain = do
-        $(logInfo) ("sendPlain instruction is comming from " <> extEventSource x)
-        case (extEventTarget x, extEventMessages x) of
-            (Just target, Just messages) ->
-                bindM2 pushMessage getAccessToken $ pure $ messageObject target messages
-            _ -> unexpectedUndefined
-    | otherwise = $(logError) "expected join event"
+    | extEventType x == ExtEventTypeSendPlain = $(logInfo) ("sendPlain instruction is comming from " <> extEventSource x)
+        >> runMaybeT ((,) <$> hoistMaybe (extEventTarget x) <*> hoistMaybe (extEventMessages x))
+        >>= maybe unexpectedUndefined (bindM2 pushMessage getAccessToken . uncurry messageObject)
+    | otherwise = $(logError) "expected sendPlain event"
         >> $(logError) (tshow x)
     where
         unexpectedUndefined = $(logError)
                 ("sendPlain requested but target or message is not defined: " <> tshow x)
-        messageObject target messages = PushMessage {
+        messageObject target messages = pure $ PushMessage {
             pmTo = target
           , pmMessages = map (\x -> MBText $ textMessage x Nothing Nothing) messages
           }

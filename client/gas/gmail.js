44047413eca09a7fa8e7f42b8dcf550fa748b753
Restyled.io <commits@restyled.io>
2023-11-14 18:49:46 +0000

    Restyled by clang-format

--- client/gas/gmail.js
+++ client/gas/gmail.js
@@ -1,27 +1,28 @@
 const getLabel = (labelName) => {
   return labelName.length == 0
-    ? null
-    : GmailApp.getUserLabelByName(
-        labelName.split("/").reduce((acc, x) => {
-          if (GmailApp.getUserLabelByName(acc) == null) {
-            GmailApp.createLabel(acc);
-          }
-          return [acc, x].join("/");
-        }),
-      );
+             ? null
+             : GmailApp.getUserLabelByName(
+                   labelName.split("/").reduce((acc, x) => {
+                     if (GmailApp.getUserLabelByName(acc) == null) {
+                       GmailApp.createLabel(acc);
+                     }
+                     return [ acc, x ].join("/");
+                   }),
+               );
 };
 
 const getMessage = (thread) => {
   const messages = GmailApp.getMessagesForThread(thread);
   return messages.length != 1
-    ? messages[messages.length - 1] // NOTE: sending last
-    : messages[0];
+             ? messages[messages.length - 1] // NOTE: sending last
+             : messages[0];
 };
 
 const getUnreadBillingMailsWith = (f) => {
   const alreadyReadLabel = "Kiirotori/TokyoGasAlreadyRead";
   const title = "【myTOKYOGAS】ご請求料金確定のお知らせ";
-  const q = ["from:members@tokyo-gas.co.jp has:nouserlabels", title].join(" ");
+  const q =
+      [ "from:members@tokyo-gas.co.jp has:nouserlabels", title ].join(" ");
   const url = "https://members.tokyo-gas.co.jp/services/mieru/total.html";
 
   const label = getLabel(alreadyReadLabel);
@@ -34,14 +35,15 @@ const getUnreadBillingMailsWith = (f) => {
 const getUnreadOneTimePassWith = (f) => {
   const alreadyReadLabel = "Kiirotori/TokyoGasAlreadyRead";
   const title = "【myTOKYOGAS】ワンタイムパスワードのお知らせ";
-  const q = ["from:members@tokyo-gas.co.jp has:nouserlabels", title].join(" ");
+  const q =
+      [ "from:members@tokyo-gas.co.jp has:nouserlabels", title ].join(" ");
 
   const label = getLabel(alreadyReadLabel);
   for (let thread of GmailApp.search(q)) {
     const body = getMessage(thread).getPlainBody();
     f(
-      body.match(/【ワンタイムパスワード】[0-9]{6}/),
-      body.match(/【有効期間】.*/),
+        body.match(/【ワンタイムパスワード】[0-9]{6}/),
+        body.match(/【有効期間】.*/),
     );
     thread.addLabel(label);
   }

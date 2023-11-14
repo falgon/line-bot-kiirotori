3244bf6e1dc751e7a96f5583dc20aca279005ef6
Restyled.io <commits@restyled.io>
2023-11-14 18:49:51 +0000

    Restyled by prettier

--- client/gas/main.js
+++ client/gas/main.js
@@ -2,26 +2,24 @@ const prop = PropertiesService.getScriptProperties().getProperties();
 
 const makeSignature = (requestBody, secretKey) => {
   return Utilities.base64Encode(
-      Utilities
-          .newBlob(
-              Utilities.computeHmacSha256Signature(
-                  Utilities.newBlob(requestBody).getBytes(),
-                  Utilities.newBlob(secretKey).getBytes(),
-                  ),
-              )
-          .getBytes(),
+    Utilities.newBlob(
+      Utilities.computeHmacSha256Signature(
+        Utilities.newBlob(requestBody).getBytes(),
+        Utilities.newBlob(secretKey).getBytes(),
+      ),
+    ).getBytes(),
   );
 };
 
 const postKiirotori = (body, secretKey) => {
   const endpoint = prop.ENDPOINT;
   const resp = UrlFetchApp.fetch(endpoint, {
-    method : "post",
-    headers : {
-      "x-ext-line-signature" : makeSignature(body, secretKey),
+    method: "post",
+    headers: {
+      "x-ext-line-signature": makeSignature(body, secretKey),
     },
-    contentType : "application/json",
-    payload : body,
+    contentType: "application/json",
+    payload: body,
   });
   return resp.getResponseCode() == 200;
 };
@@ -30,32 +28,31 @@ const sendLineNotify = (message) => {
   const endpoint = "https://notify-api.line.me/api/notify";
   const token = prop.LINE_NOTIFY_TOKEN;
   UrlFetchApp.fetch(endpoint, {
-    method : "post",
-    headers : {
-      Authorization : [ "Bearer", token ].join(" "),
+    method: "post",
+    headers: {
+      Authorization: ["Bearer", token].join(" "),
     },
-    payload : {
-      message : message,
+    payload: {
+      message: message,
     },
   });
 };
 
 const sendMessages = (s) => {
   const eventObject = {
-    type : "sendPlain",
-    timestamp : new Date().getMilliseconds(),
-    source : "Google Apps Script - TokyoGASKiirotoriAutoTransfer",
-    webhookEventId :
-        ULID.ulid(), // thx: https://zenn.dev/gyumesy/articles/a3adc6110859ac
-    deliveryContext : {
-      isRedelivery : false,
+    type: "sendPlain",
+    timestamp: new Date().getMilliseconds(),
+    source: "Google Apps Script - TokyoGASKiirotoriAutoTransfer",
+    webhookEventId: ULID.ulid(), // thx: https://zenn.dev/gyumesy/articles/a3adc6110859ac
+    deliveryContext: {
+      isRedelivery: false,
     },
-    messages : s,
-    target : prop.FAMILY_GROUP_ID,
+    messages: s,
+    target: prop.FAMILY_GROUP_ID,
   };
   const requestBody = {
-    destination : prop.BOT_USER_ID,
-    events : [ eventObject ],
+    destination: prop.BOT_USER_ID,
+    events: [eventObject],
   };
 
   try {
